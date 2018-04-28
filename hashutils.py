bb4d7027dd39bdd6b9dbd063a8b868b3a9db783b
phudinhn <phudinhn@gmail.com>
2018-04-28 13:28:29 -0500

    Add hashing function

---
+++ hashutils.py
@@ -0,0 +1,19 @@
+import hashlib
+import random
+import string
+
+def make_salt():
+    return ''.join([random.choice(string.ascii_letters) for x in range (5)])
+
+def make_pw_hash(password, salt=None):
+    if not salt:
+        salt = make_salt()
+    pw_hash = hashlib.sha256(str.encode(password)).hexdigest()
+    return '{0}, {1}'.format(pw_hash, salt)
+
+def check_pw_hash(password, pw_hash):
+    salt = pw_hash.split(', ')[1]
+    if make_pw_hash(password, salt) == pw_hash:
+        return True
+    
+    return False
